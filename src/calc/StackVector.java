59468bf580a537a292297a198c92c7e24a868a76
L2Dev <orellanasolares.luis@gmail.com>
2014-07-18 07:56:47 -0600

    Creación de la clase StackVector

---
+++ src/calc/StackVector.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calc;
+
+/**
+ *
+ * @author Konoha
+ */
+public class StackVector implements Stack{
+
+    @Override
+    public void push(Object item) {
+        
+    }
+
+    @Override
+    public Object pop() {
+        
+    }
+
+    @Override
+    public Object peek() {
+        
+    }
+
+    @Override
+    public boolean empty() {
+        
+    }
+
+    @Override
+    public int size() {
+        
+    }
+    
+}
